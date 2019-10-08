e0d65751d846ff6ccf1a4745d910fb6434ac72dd
Ubuntu <ubuntu@ip-172-31-40-163.ec2.internal>
2019-10-08 21:15:46 +0000

    change config to match ec2 setting

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "mysql://ece1779a1:password123@localhost/ece1779a1"
 
-    IMAGES_UPLOAD_URL = "/Users/ranyang/Desktop/ece1779a1"
+    IMAGES_UPLOAD_URL = "/home/ubuntu/Desktop/uploaded_photo"
     ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG"]
     MAX_IMAGE_SIZE = 0.5 * 1024 * 1024
 
