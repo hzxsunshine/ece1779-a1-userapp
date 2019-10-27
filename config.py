669915f911210e62321ba2c26ad9e22403062a02
ryang <rita.yang@sentaca.com>
2019-10-27 01:22:29 -0400

    config rds

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config(object):
     SECRET_KEY = "fe8e5c349e8eb13bf65bdc261229d43d"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql://ece1779a1:password123@localhost/ece1779a1"
+    SQLALCHEMY_DATABASE_URI = \
+        "mysql://ece1779a2:password123@ece1779a2.csmeodxl9uyw.us-east-1.rds.amazonaws.com/ece1779a2"
 
     TEXT_DETECTION_PB_PATH = "/static/east/frozen_east_text_detection.pb"
     ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG"]
